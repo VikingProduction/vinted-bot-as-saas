66a0a7817cf80e0260d60db50e566be315590df5
Viking Production <154587935+VikingProduction@users.noreply.github.com>
2025-09-17 14:02:12 +0200

    Create users.py

---
+++ backend/app/routers/users.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends, HTTPException
+from ..models.user import User
+from ..database import get_db
+from sqlalchemy.orm import Session
+from ..auth.auth_bearer import JWTBearer
+
+router = APIRouter(tags=["users"], prefix="/users", dependencies=[Depends(JWTBearer())])
+
+@router.get("/me")
+def read_current_user(db: Session = Depends(get_db), token_data=Depends(JWTBearer())):
+    user = db.query(User).filter(User.username == token_data["sub"]).first()
+    if not user:
+        raise HTTPException(404, "Utilisateur non trouvé")
+    return {
+        "id": user.id,
+        "email": user.email,
+        "username": user.username,
+        "subscription_plan": user.subscription_plan,
+    }
