21a155d55c31aa3c99f8df54194ea150172e7e80
Viking Production <154587935+VikingProduction@users.noreply.github.com>
2025-09-18 11:03:47 +0200

    Create index.ts

---
+++ frontend/src/utils/index.ts
@@ -0,0 +1,29 @@
+// Utilitaires pour l'application
+
+export const formatPrice = (price: number): string => {
+  return new Intl.NumberFormat('fr-FR', {
+    style: 'currency',
+    currency: 'EUR'
+  }).format(price);
+};
+
+export const formatDate = (date: string): string => {
+  return new Intl.DateTimeFormat('fr-FR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
+  }).format(new Date(date));
+};
+
+export const debounce = <T extends (...args: any[]) => any>(
+  func: T,
+  delay: number
+): ((...args: Parameters<T>) => void) => {
+  let timeoutId: NodeJS.Timeout;
+  return (...args: Parameters<T>) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func(...args), delay);
+  };
+};
