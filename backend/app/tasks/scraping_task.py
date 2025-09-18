7eb6cbe4169781ce7becd9db9844582767348abc
Viking Production <154587935+VikingProduction@users.noreply.github.com>
2025-09-18 18:36:00 +0200

    Update scraping_task.py

--- backend/app/tasks/scraping_task.py
+++ backend/app/tasks/scraping_task.py
@@ -1,6 +1,6 @@
 from .celery_app import celery_app
 from sqlalchemy.orm import Session
-from ..db import SessionLocal
+from ..core.database import SessionLocal
 from ..scraping.vinted_client import check_filters_and_notify
 
 @celery_app.task(name="backend.app.tasks.scraping_task.run_all_filters", ignore_result=True)
